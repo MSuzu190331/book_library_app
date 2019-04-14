27ddc34b82ccdc58269603fb2d485985498a34a4
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-04-14 14:59:37 +0900

    view new&edit作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # get 'home/index'
    get "/" => "home#index"
    get "about" => "home#about"
+   get "books/index" => "books#index"
+   get "comics/index" => "comics#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
