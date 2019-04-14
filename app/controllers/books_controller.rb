f0c57636b3445bc56444a5ae9fb6c2c8e5bd3622
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-04-14 22:15:23 +0900

    initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+# ＃下記動かせば正常に動く
+  # def create
+  #   @book = Book.create(book_params)
+  #   redirect_to books_index_path
+  # end
+  #
+  # def show
+  # end
+
+  # private
+  # def book_params
+  #   params.require(:book).permit(:title)
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   # get 'home/index'
-   get "/" => "home#index"
-   get "about" => "home#about"
-   get "books/index" => "books#index"
-   get "comics/index" => "comics#index"
+   # get "/" => "home#index"
+   # get "about" => "home#about"
+   # get "books/index" => "books#index"
+   # get "comics/index" => "comics#index"
+   # post "books/new" => "books#new"
+   # get "books/new" => "books#new"
+   # resources :home
+   # resources :comics
+   root 'home#index'
+   get 'books/index'
+   get 'books/new'　
+   post 'books/create'　　#このPOST動かせば正常に動く　
+   get 'books/edit'
+   get 'comics/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
